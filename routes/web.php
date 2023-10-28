04179e01790f6a8188ed6afdc2dbb80fe75d9d7b
Paul <ahmadzufukli@gmail.com>
2023-10-28 10:08:22 +0800

    practicing route

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\ReaderController;
+use App\Http\Controllers\DashboardController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -35,3 +26,9 @@ require __DIR__.'/auth.php';
 Route::get('/book', [BookController::class, 'index'])->name('books.index');
 
 Route::get('/reader', [ReaderController::class, 'index'])->name('readers.read');
+
+Route::get('/hello', function() {
+    return 'Hello From Laravel';
+});
+
+Route::get('/home', [DashboardController::class, 'index']);
